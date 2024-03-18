ccab67950afd51dea47e88f77ce6c74888a607c6
mellymell-appdev4ever <157563824+mellymell-appdev4ever@users.noreply.github.com>
2024-03-18 21:04:09 +0800

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ st.write(
     """Breakfast menu
     """
 )
+st.write("Omega 3 & Blueberry Oatmeal")
+st.write("Kale, Spinach and Rocket Smoothie")
+st.write("Hard-Boiled Free-Range Egg")
+
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your order will be: ', name_on_order)
 
